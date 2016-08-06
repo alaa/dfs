7942565cd8ede768b5eda4404a9d04649054845b
Alaa Qutaish <alaa.qutaish@gmail.com>
2016-08-06 13:12:15 +0200

    use native types in splitter and add mergeparts

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 
@@ -19,4 +20,19 @@ func main() {
 	if err = splitter.WriteParts(parts); err != nil {
 		log.Println(err)
 	}
+
+	p1, err := ioutil.ReadFile("./0")
+	if err != nil {
+		log.Panic("could not read file")
+	}
+	p2, err := ioutil.ReadFile("./1")
+	if err != nil {
+		log.Panic("could not read file")
+	}
+
+	var file [][]byte
+	file = append(file, p1)
+	file = append(file, p2)
+	f := splitter.MergeParts(file)
+	fmt.Printf("assembled file size is %s", len(f))
 }
--- splitter/splitter.go
+++ splitter/splitter.go
@@ -6,31 +6,37 @@ import (
 	"os"
 )
 
-type part struct {
-	data []byte
+func MergeParts(Parts [][]byte) []byte {
+	var file []byte
+	for _, part := range Parts {
+		for _, data := range part {
+			file = append(file, data)
+		}
+	}
+	return file
 }
 
-func WriteParts(parts []part) error {
+func WriteParts(Parts [][]byte) error {
 	var buf bytes.Buffer
-	for i, part := range parts {
+	for i, part := range Parts {
 		f, err := newFile(fmt.Sprintf("%d", i))
 		defer f.Close()
 		if err != nil {
 			return err
 		}
-		buf.Write(part.data)
+		buf.Write(part)
 		buf.WriteTo(f)
 	}
 	return nil
 }
 
-func Split(bytes []byte, chunkSize int) []part {
-	var parts []part
+func Split(bytes []byte, chunkSize int) [][]byte {
+	var Parts [][]byte
 	for i := 0; i <= len(bytes); i += chunkSize {
 		chunk := bytes[i:min(i+chunkSize, len(bytes))]
-		parts = append(parts, part{data: chunk})
+		Parts = append(Parts, chunk)
 	}
-	return parts
+	return Parts
 }
 
 func newFile(filename string) (*os.File, error) {
